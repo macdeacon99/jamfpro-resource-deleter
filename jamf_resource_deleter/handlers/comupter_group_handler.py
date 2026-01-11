14e09b124e10a01618f40919d1df56e677bf809e
Gordon Deacon <gdeacon99@gmail.com>
2026-01-11 12:09:37 +0000

    feat: add computer group restore

--- jamf_resource_deleter/handlers/comupter_group_handler.py
+++ jamf_resource_deleter/handlers/comupter_group_handler.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Optional, Dict
+from dicttoxml import dicttoxml
 from requests import HTTPError
 from .base import ResourceHandler
 
@@ -20,3 +21,21 @@ class ComputerGroupHandler(ResourceHandler):
                 "Could not retrieve %s %s: %s", self.resource_name, resource_id, e
             )
             return None
+
+    def create(self, resource_config: Dict) -> bool:
+        xml = self._convert_to_xml(resource_config)
+
+        try:
+            success = self.client.classic.computer_groups.create(xml)
+
+            return success.ok, success.status_code
+        except HTTPError as e:
+            logger.error("Error: %s", e)
+            return success.ok, success.status_code
+
+    def _convert_to_xml(self, resource_config):
+        ee_data = resource_config["computer_groups"]
+
+        return dicttoxml(
+            ee_data, custom_root="computer_extension_attribute", attr_type=False
+        )
