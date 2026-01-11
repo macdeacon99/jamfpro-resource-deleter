5977cfb07d62e315aaf062fcb92e3e4628ef5274
Gordon Deacon <gdeacon99@gmail.com>
2026-01-11 15:02:59 +0000

    fix: change endpoint

--- jamf_resource_deleter/handlers/restricted_software_handler.py
+++ jamf_resource_deleter/handlers/restricted_software_handler.py
@@ -26,8 +26,7 @@ class RestrictedSoftwareHandler(ResourceHandler):
         xml = self._convert_to_xml(resource_config)
 
         try:
-            success = self.client.classic.policies.create(xml)
-            print(success.text)
+            success = self.client.classic.restricted_software.create(xml)
             return success.ok, success.status_code
         except HTTPError as e:
             logger.error("Error: %s", e)
