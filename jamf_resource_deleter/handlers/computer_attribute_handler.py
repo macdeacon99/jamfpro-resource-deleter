260454fae17341ecf4fac3a60d22a6db52b278d1
Gordon Deacon <gdeacon99@gmail.com>
2026-01-11 11:43:18 +0000

    chore: format

--- jamf_resource_deleter/handlers/computer_attribute_handler.py
+++ jamf_resource_deleter/handlers/computer_attribute_handler.py
@@ -29,7 +29,6 @@ class ComputerAttributeHandler(ResourceHandler):
             return None
 
     def create(self, resource_config: Dict) -> bool:
-
         xml = self._convert_to_xml(resource_config)
 
         try:
