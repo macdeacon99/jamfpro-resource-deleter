080be8bc65ddffc037cbfab9c4f828320ba41518
Gordon Deacon <gdeacon99@gmail.com>
2026-01-11 12:18:33 +0000

    fix: wrong key

--- jamf_resource_deleter/handlers/comupter_group_handler.py
+++ jamf_resource_deleter/handlers/comupter_group_handler.py
@@ -34,7 +34,7 @@ class ComputerGroupHandler(ResourceHandler):
             return success.ok, success.status_code
 
     def _convert_to_xml(self, resource_config):
-        ee_data = resource_config["computer_groups"]
+        ee_data = resource_config["unusedComputerGroups"]
 
         return dicttoxml(
             ee_data, custom_root="computer_extension_attribute", attr_type=False
